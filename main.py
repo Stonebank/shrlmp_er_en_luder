6161cc3b613ca56567d7c231756069dfbf01cbf1
stonebank <35108449>
2020-08-27 23:23:17 +0200

    important update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import subprocess
+import re
 
 from selenium import webdriver
 
@@ -11,6 +12,10 @@ time.sleep(2)
 browser.find_element_by_link_text("Old School").click()
 time.sleep(2)
 
+players = re.sub('[^0-9]', '', browser.find_element_by_class_name("player-count").text)
+
+subprocess.run(["say", f"Der er {players} på Oldschool RuneScape in my dick"])
+
 browser.find_element_by_link_text("HiScores").click()
 time.sleep(2)
 
@@ -18,4 +23,4 @@ browser.find_element_by_name("user1").send_keys("shrlmp")
 browser.find_element_by_name("submit").click()
 time.sleep(2)
 
-subprocess.run(["say", "Shrlmp er en luder"])
+subprocess.run(["say", "Jasser er en luder"])
